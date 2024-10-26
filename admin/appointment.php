7fd1097fd96fd20085746f73524e7b20acc3ddbb
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-10-26 20:26:25 +0530

    Update appointment.php

--- admin/appointment.php
+++ admin/appointment.php
@@ -190,7 +190,7 @@
                 
                 <?php
                     if($_POST){
-                        //print_r($_POST);
+                      
                         $sqlpt1="";
                         if(!empty($_POST["sheduledate"])){
                             $sheduledate=$_POST["sheduledate"];
