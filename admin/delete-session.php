7859a9b3762d76f6f2db68ba56f00cff0740bae6
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-11-04 00:28:58 +0530

    Update delete-session.php

--- admin/delete-session.php
+++ admin/delete-session.php
@@ -17,7 +17,7 @@
         include("../connection.php");
         $id=$_GET["id"];
        
-        //$email=($result001->fetch_assoc())["docemail"];
+    
         $sql= $database->query("delete from schedule where scheduleid='$id';");
         //$sql= $database->query("delete from doctor where docemail='$email';");
         //print_r($email);
